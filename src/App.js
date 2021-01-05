a9ea42534374aa7fafb1319e7d25e42761a76923
bivek520 <57966410+bivek520@users.noreply.github.com>
2021-01-05 10:08:17 -0600

    added routes and axios package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import LocationForm from './component/locationForm';
+
+import './App.css';
 function App() {
 	return (
 		<React.Fragment>
 			<Switch>
 				<Route path='/check_your_air' component={LocationForm} />
+				<Redirect from='/' exact to='/check_your_air' />
 			</Switch>
 		</React.Fragment>
 	);
--- src/component/locationForm.jsx
+++ src/component/locationForm.jsx
@@ -1,10 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Form from './common/form';
+import Joi from 'joi-browser';
 
-class LocationForm extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+class LocationForm extends Form {
+	state = {
+		data: { location: '' },
+		errors: {},
+	};
+
+	schema = {
+		location: Joi.string().required().label('Location'),
+	};
+	doSubmit = () => {
+		// Call the server
+		console.log('Submitted');
+	};
+	render() {
+		return (
+			<div>
+				<h1>Check Your Air quality</h1>
+				<form onSubmit={this.handleSubmit}>
+					{this.renderInput('location', 'Location')}
+					{this.renderButton('Check')}
+				</form>
+			</div>
+		);
+	}
 }
- 
+
 export default LocationForm;
